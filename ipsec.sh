2dac4aa56c9ea17400db759cbd130ab44aee1628
tz1006 <a@omg.tf>
2017-03-24 18:42:41 -0700

    Update ipsec.sh

--- ipsec.sh
+++ ipsec.sh
@@ -1,11 +1,12 @@
-# install ipsec server
-# rm -rf /etc/strongswan/ipsec.conf /etc/strongswan/ipsec.secrets /etc/strongswan/strongswan.conf
+#!/bash/sh
+yum -y install git wget
+git clone https://github.com/tz1006/PPTP-IPsec.git ~/ipsec
 yum -y install strongswan
+# rm -rf /etc/strongswan/ipsec.conf /etc/strongswan/ipsec.secrets /etc/strongswan/strongswan.conf
 \cp -f /root/ipsec/ipsec.conf /etc/strongswan/ipsec.conf
 \cp -f /root/ipsec/ipsec.secrets /etc/strongswan/ipsec.secrets
 \cp -f /root/ipsec/strongswan.conf /etc/strongswan/strongswan.conf
 bash ~/ipsec/sysctl.sh
-bash ~/ipsec/iptables.sh
-
+bash ~/iptables.sh
 service strongswan start
 chkconfig strongswan on
