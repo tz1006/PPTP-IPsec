c48dbeab68ebeb09d1ccdc8b9e5c7164f06305fb
tz1006 <a@omg.tf>
2017-03-24 18:07:04 -0700

    Update ipsec.sh

--- ipsec.sh
+++ ipsec.sh
@@ -1,5 +1,6 @@
 # install ipsec server
 # rm -rf /etc/strongswan/ipsec.conf /etc/strongswan/ipsec.secrets /etc/strongswan/strongswan.conf
+yum -y install strongswan
 \cp -f /root/ipsec/ipsec.conf /etc/strongswan/ipsec.conf
 \cp -f /root/ipsec/ipsec.secrets /etc/strongswan/ipsec.secrets
 \cp -f /root/ipsec/strongswan.conf /etc/strongswan/strongswan.conf
