8662d49cfaf88e78d88f6f3058857064694a43d4
tz1006 <a@omg.tf>
2017-03-24 21:00:05 -0700

    Update and rename ipsec.sh to START.sh

--- START.sh
+++ START.sh
@@ -4,16 +4,16 @@ git clone https://github.com/tz1006/PPTP-IPsec.git ~/PPTP-IPsec
 
 # Intall PPTP
 yum install -y pptpd
-\cp -f /root/PPTP-IPsec/options.pptpd /etc/ppp/options.pptpd
-\cp -f /root/PPTP-IPsec/chap-secrets /etc/ppp/chap-secrets
-\cp -f /root/PPTP-IPsec/pptpd.conf /etc/pptpd.conf
+\cp -f ~/PPTP-IPsec/options.pptpd /etc/ppp/options.pptpd
+\cp -f ~/PPTP-IPsec/chap-secrets /etc/ppp/chap-secrets
+\cp -f ~/PPTP-IPsec/pptpd.conf /etc/pptpd.conf
 
 # IPsec
 yum -y install strongswan
 # rm -rf /etc/strongswan/ipsec.conf /etc/strongswan/ipsec.secrets /etc/strongswan/strongswan.conf
-\cp -f /root/PPTP-IPsec/ipsec.conf /etc/strongswan/ipsec.conf
-\cp -f /root/PPTP-IPsec/ipsec.secrets /etc/strongswan/ipsec.secrets
-\cp -f /root/PPTP-IPsec/strongswan.conf /etc/strongswan/strongswan.conf
+\cp -f ~/PPTP-IPsec/ipsec.conf /etc/strongswan/ipsec.conf
+\cp -f ~/PPTP-IPsec/ipsec.secrets /etc/strongswan/ipsec.secrets
+\cp -f ~/PPTP-IPsec/strongswan.conf /etc/strongswan/strongswan.conf
 
 # Systerm Configration
 bash ~/PPTP-IPsec/sysctl.sh
