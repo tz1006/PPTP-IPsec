d5e8420f7a9e50b27d15c4abb6eaf66d70a7d41a
tz1006 <a@omg.tf>
2017-03-24 20:52:39 -0700

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -1,6 +1,6 @@
 #! ／bash/sh
 
-echo net.ipv4.ip_forward = 1 >> /etc/sysctl.conf
+sed -i 's/^net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
 echo net.ipv4.conf.all.accept_redirects = 0 >> /etc/sysctl.conf
 echo net.ipv4.conf.all.send_redirects = 0 >> /etc/sysctl.conf
 
