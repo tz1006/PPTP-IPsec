007620484d0c5682c5f12ac3c8c579d2c1c67e84
tz1006 <a@omg.tf>
2017-03-24 17:56:56 -0700

    Create ipsec.sh

---
+++ ipsec.sh
@@ -0,0 +1,15 @@
+#!/bash/sh
+yum -y install git wget
+
+yum -y install strongswan
+mkdir ~/ipsec
+git clone https://github.com/tz1006/PPTP-IPsec.git ~/ipsec
+# rm -rf /etc/strongswan/ipsec.conf /etc/strongswan/ipsec.secrets /etc/strongswan/strongswan.conf
+\cp -f /root/ipsec/ipsec.conf /etc/strongswan/ipsec.conf
+\cp -f /root/ipsec/ipsec.secrets /etc/strongswan/ipsec.secrets
+\cp -f /root/ipsec/strongswan.conf /etc/strongswan/strongswan.conf
+bash ~/ipsec/sysctl.sh
+bash ~/iptables.sh
+
+service strongswan start
+chkconfig strongswan on
