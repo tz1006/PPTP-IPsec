43e97f5b2a3c605669e5ca75bb40b510aa7fbcb3
tz1006 <a@omg.tf>
2017-03-24 20:50:37 -0700

    Update ipsec.sh

--- ipsec.sh
+++ ipsec.sh
@@ -1,12 +1,24 @@
 #!/bash/sh
 yum -y install git wget
-git clone https://github.com/tz1006/PPTP-IPsec.git ~/ipsec
+git clone https://github.com/tz1006/PPTP-IPsec.git ~/PPTP-IPsec
+
+# Intall PPTP
+yum install -y pptpd
+\cp -f /root/PPTP-IPsec/options.pptpd /etc/ppp/options.pptpd
+\cp -f /root/PPTP-IPsec/chap-secrets /etc/ppp/chap-secrets
+\cp -f /root/PPTP-IPsec/pptpd.conf /etc/pptpd.conf
+
+# IPsec
 yum -y install strongswan
 # rm -rf /etc/strongswan/ipsec.conf /etc/strongswan/ipsec.secrets /etc/strongswan/strongswan.conf
-\cp -f /root/ipsec/ipsec.conf /etc/strongswan/ipsec.conf
-\cp -f /root/ipsec/ipsec.secrets /etc/strongswan/ipsec.secrets
-\cp -f /root/ipsec/strongswan.conf /etc/strongswan/strongswan.conf
-bash ~/ipsec/sysctl.sh
-bash ~/ipsec/iptables.sh
+\cp -f /root/PPTP-IPsec/ipsec.conf /etc/strongswan/ipsec.conf
+\cp -f /root/PPTP-IPsec/ipsec.secrets /etc/strongswan/ipsec.secrets
+\cp -f /root/PPTP-IPsec/strongswan.conf /etc/strongswan/strongswan.conf
+
+# Systerm Configration
+bash ~/PPTP-IPsec/sysctl.sh
+bash ~/PPTP-IPsec/iptables.sh
+service pptpd start
+chkconfig pptpd on
 service strongswan start
 chkconfig strongswan on
