5371cd478a6bc81484736f42f8ddad61b1b19d90
tz1006 <a@omg.tf>
2019-01-25 07:17:34 +0800

    Update IKEv2.sh

--- IKEv2.sh
+++ IKEv2.sh
@@ -1,19 +1,28 @@
 #!/bash/sh
+
+
+# ----设定变量----
+domain = @1
+# ---------------
+
+
+
+# Prerequisites
 sudo apt-get update -y
-sudo apt-get install -y sysv-rc-conf
-git https://github.com/tz1006/PPTP-IPsec.git
+sudo apt-get install -y strongswan strongswan-pkl centbot
+#sudo apt-get install -y sysv-rc-conf
 
-# Install PPTP
-sudo apt-get install -y pptpd
-sudo cp -f ~/PPTP-IPsec/options.pptpd /etc/ppp/options.pptpd
-sudo cp -f ~/PPTP-IPsec/chap-secrets /etc/ppp/chap-secrets
-sudo cp -f ~/PPTP-IPsec/pptpd.conf /etc/pptpd.conf
+# Apply Cert
+sudo certbot certonly --standalone --agree-tos \
+                   --no-eff-email \
+                   --email hakase@gmail.com \
+                   -d ikev2.hakase-labs.io
 
-# Install IPsec
-sudo apt-get install -y strongswan
-sudo cp -f ~/PPTP-IPsec/ipsec.conf /etc/ipsec.conf
-sudo cp -f ~/PPTP-IPsec/ipsec.secrets /etc/ipsec.secrets
-sudo cp -f ~/PPTP-IPsec/strongswan.conf /etc/strongswan.conf
+# Config Ipsec
+sudo wget  -O /etc/ipsec.secrets
+sudo wget  -O  /etc/strongswan.d/strongswan.conf
+
+
 
 # Sysctl Config
 sudo sed -i 's/^net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
@@ -22,12 +31,11 @@ sudo sysctl -p
 # Firewall
 sudo ufw allow 500
 sudo ufw allow 4500
-sudo ufw allow 4500
 sudo ufw allow 1701
 sudo ufw allow 1723
 
-# Start
-sudo service pptpd start
-sudo sysv-rc-conf pptpd on
-sudo service strongswan start
-sudo sysv-rc-conf strongswan on
+# restart
+#sudo service pptpd start
+#sudo sysv-rc-conf pptpd on
+sudo service strongswan restart
+#sudo sysv-rc-conf strongswan on
