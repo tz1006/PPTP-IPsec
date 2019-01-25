8b792a1790fc8de1463d5a679a53e25c4480fce8
tz1006 <a@omg.tf>
2019-01-25 16:21:54 +0800

    Update IKEv2.sh

--- IKEv2.sh
+++ IKEv2.sh
@@ -1,17 +1,14 @@
 #!/bash/sh
 
-
 # ----设定变量----
-domain=$(hostname)
-IP=$(hostname -I)
+#domain=$(hostname)
+#IP=$(hostname -I)
 #NetCard=$(ifconfig|grep "^en"|awk '{print $1}')
 #NetCard=${NetCard::-1}
-NetCard=$(ls /sys/class/net/ |grep "^en")
+#NetCard=$(ls /sys/class/net/ |grep "^en")
 #CertPath="/etc/letsencrypt/live/$(hostname)/fullchain.pem"
 #KeyPath="/etc/letsencrypt/live/$(hostname)/privkey.pem"
 # ---------------
-
-
 
 # Prerequisites
 sudo apt-get update -y
@@ -19,12 +16,9 @@ sudo apt-get install -y strongswan strongswan-pki libcharon-standard-plugins lib
 sudo apt-get install -y certbot
 #sudo apt-get install -y sysv-rc-conf
 
-# Startup Script
-curl -s https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/rc.sh | bash 
-
 # Disable AppArmor
-#sudo systemctl stop apparmor
-#sudo systemctl disable apparmor
+sudo systemctl stop apparmor
+sudo systemctl disable apparmor
 #sudo /etc/init.d/apparmor stop
 #sudo update-rc.d -f apparmor remove
 
@@ -44,10 +38,9 @@ sudo wget https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/config/ipse
 sed -i "s/example.com/$(hostname)/g" /etc/ipsec.conf
 sed -i "s/example.com/$(hostname)/g" /etc/ipsec.secrets
 
-
 # Sysctl Config
 sed -i '/net.ipv4.ip_forward/c net.ipv4.ip_forward = 1' /etc/sysctl.conf
-sudo sysctl -p
+#sudo sysctl -p
 
 # Firewall
 #sudo iptables -X
@@ -58,14 +51,18 @@ sudo ufw allow 4500
 sudo ufw allow 1701
 #sudo ufw allow 1723
 #iptables -t nat -A POSTROUTING -s 10.99.1.0/24 -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE
-sed '4 iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE' -i /etc/rc.local
+#iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE
+
+# Startup Script
+curl -s https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/rc.sh | bash 
+sed '3a iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE' -i /etc/rc.local
 
 #sudo iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
 
 
 # restart
+reboot
 #sudo service pptpd start
 #sudo sysv-rc-conf pptpd on
-sudo service ipsec restart
+#sudo service ipsec restart
 #sudo sysv-rc-conf strongswan on
