4bc4877270920e8fa432b080e8caf6481e27b818
tz1006 <a@omg.tf>
2019-01-25 15:23:57 +0800

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,11 @@
+# 
+
+sudo ln -fs /lib/systemd/system/rc-local.service /etc/systemd/system/rc-local.service  
+
+sudo echo '#!/bin/sh -e
+# 
+# rc.local
+
+exit 0' > /etc/rc.local
+
+sudo chmod 755 /etc/rc.local 
