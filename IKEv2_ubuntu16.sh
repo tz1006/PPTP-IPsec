b354618b8952f637db17de40c88921b14bc56add
tz1006 <a@omg.tf>
2019-01-25 17:34:37 +0800

    Update IKEv2_ubuntu16.sh

--- IKEv2_ubuntu16.sh
+++ IKEv2_ubuntu16.sh
@@ -8,6 +8,12 @@
 sudo apt-get update -y
 sudo apt-get install -y strongswan libstrongswan-standard-plugins letsencrypt
 
+# Disable AppArmor
+sudo systemctl stop apparmor
+sudo systemctl disable apparmor
+#sudo /etc/init.d/apparmor stop
+#sudo update-rc.d -f apparmor remove
+
 # Apply Cert
 wget https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem -O /etc/ipsec.d/cacerts/cacert.pem
 sudo letsencrypt certonly --standalone --agree-tos \
@@ -44,13 +50,13 @@ sudo ufw allow 1701
 
 # Startup Script
 curl -s https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/rc.sh | bash 
-#sed '3a iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE' -i /etc/rc.local
+sed '3a iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE' -i /etc/rc.local
 
 #sudo iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
 
 
 # restart
-#reboot
+reboot
 #sudo service pptpd start
 #sudo sysv-rc-conf pptpd on
 #sudo service ipsec restart
