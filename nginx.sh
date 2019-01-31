5e3f02e79150a29600a67c073802b108cd9e501e
tz1006 <a@omg.tf>
2019-01-31 17:02:20 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -8,7 +8,9 @@ curl -s https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/letsencrypt.s
 
 
 rm /etc/nginx/sites-enabled/default
-wget https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/example.com -O /etc/nginx/sites-enabled/example.com
+#wget https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/example.com -O /etc/nginx/sites-enabled/example.com
+
+wget https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/nginx/airchart -O /etc/nginx/sites-enabled/example.com
 # Replace example.com
 sed -i "s/example.com/$1/g" /etc/nginx/sites-enabled/example.com
 # Replace proxy.com
