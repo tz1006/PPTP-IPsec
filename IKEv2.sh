3d29def883a7ebf6d2a22463eb56bc721b09456b
tz1006 <a@omg.tf>
2019-02-01 16:11:26 +0800

    Update IKEv2.sh

--- IKEv2.sh
+++ IKEv2.sh
@@ -57,7 +57,7 @@ sudo ufw allow 1701
 # Startup Script
 curl -s https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/rc.sh | bash 
 sed '3a iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE' -i /etc/rc.local
-
+sudo ufw -f disable && sudo ufw -f enable
 #sudo iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
 
 
