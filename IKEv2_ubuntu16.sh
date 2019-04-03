bc5fac982479808c619cf473349b17b368b260b8
tz1006 <a@omg.tf>
2019-04-03 10:01:51 +0800

    Update IKEv2_ubuntu16.sh

--- IKEv2_ubuntu16.sh
+++ IKEv2_ubuntu16.sh
@@ -33,27 +33,6 @@ sed -i "s/example.com/$(hostname)/g" /etc/ipsec.secrets
 sed -i "s/certbot/letsencrypt/g" /etc/ipsec.conf
 sed -i "s/certbot/letsencrypt/g" /etc/ipsec.secrets
 
-# Sysctl Config
-sed -i '/net.ipv4.ip_forward/c net.ipv4.ip_forward = 1' /etc/sysctl.conf
-#sudo sysctl -p
-
-# Firewall
-#sudo iptables -X
-sudo ufw allow 22
-sudo ufw allow 53
-sudo ufw allow 80
-sudo ufw allow 500
-sudo ufw allow 4500
-sudo ufw allow 1701
-#sudo ufw allow 1723
-#iptables -t nat -A POSTROUTING -s 10.99.1.0/24 -j MASQUERADE
-#iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE
-ufw -f disable && ufw -f enable
-# Startup Script
-curl -s https://raw.githubusercontent.com/tz1006/PPTP-IPsec/master/rc.sh | bash 
-sed '3a iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -j MASQUERADE' -i /etc/rc.local
-
-#sudo iptables -t nat -A POSTROUTING -o ens33 -j MASQUERADE
 
 
 # restart
